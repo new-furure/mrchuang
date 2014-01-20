2ec45953f1f72907f2c275be1a949c7036c62796
NewFuture <new_future@msn.cn>
2014-01-20 11:12:56 +0800

    change home

--- Home/Conf/config.php
+++ Home/Conf/config.php
@@ -1,5 +1,17 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+	// 添加数据库配置信息
+'DB_TYPE'   => 'mysql', // 数据库类型
+'DB_HOST'   => 'db4free.net', // 服务器地址
+'DB_NAME'   => 'newfuturedb', // 数据库名
+'DB_USER'   => 'newfuture', // 用户名
+'DB_PWD'    => 'c8714e', // 密码
+'DB_PORT'   => 3306, // 端口
+'DB_PREFIX' => '', // 数据库表前缀
+
+//'配置项'=>'配置值'
+'URL_CASE_INSENSITIVE' =>true,//不区分大小写
+'SHOW_PAGE_TRACE' =>true, // 显示页面Trace信息
+
 );
 ?>
--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -1,7 +1,49 @@
 <?php
-// 本类由系统自动生成，仅供测试用途
+/*
++------------------------------------------------
++				默认模块
++ 初稿 NewFuture
++ 完善 
++-------------------------------------------------
+*/
 class IndexAction extends Action {
+	
+	//默认页面(游客首页)
+	//此页需要验证是否已经登陆
+	//或存在cookie
+    //@作者：
     public function index(){
-	$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+        $this->display();
     }
+
+
+    //搜索页
+    //@作者：
+    public function  search()
+    {
+
+    }
+
+    //风投页
+    //@作者：
+    public function vc()
+    {
+
+    }
+
+    //介绍页
+    //@作者：
+    public function  about()
+    {
+
+    }
+
+    //网站地图
+    //所有页面连接
+    //@作者：
+    public function sitemap()
+    {
+
+    }
+
 }
