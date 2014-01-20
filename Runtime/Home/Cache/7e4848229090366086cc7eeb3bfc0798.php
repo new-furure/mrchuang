e66853e7a26664c76538024790153ac73aae8510
NewFuture <new_future@msn.cn>
2014-01-20 11:03:39 +0800

    add tp

--- Runtime/Home/Cache/7e4848229090366086cc7eeb3bfc0798.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?> 网站首页
- <head>
-   <title>MR.Chuang-<?php echo ($title); ?></title>
- </head>
-
- <body>
-	<a href="<?php echo ($href); ?>"><?php echo ($word); ?></a>
- </body>
